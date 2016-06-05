9e1d0d8b414aac7cd93dbe83cf50f50b6c6bf86c
Will Soto <will.soto9@gmail.com>
2016-06-05 19:44:48 -0400

    test(chartist.component): add more tests
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- test/chartist.component.spec.ts
+++ test/chartist.component.spec.ts
@@ -14,27 +14,28 @@ import {
 
 import * as Chartist from 'chartist';
 
-import { ChartistComponent } from './../angular2-chartist';
+import { ChartistComponent, ChartType, ChartEvent } from './../angular2-chartist';
 
 const data: any = require('./data.json');
 
-describe('chartist component', function() {
+describe('chartist component', function(): void {
   let builder: TestComponentBuilder;
 
-  beforeEach(inject([TestComponentBuilder], function(tcb) {
+  beforeEach(inject([TestComponentBuilder], function(tcb): void {
     builder = tcb;
   }));
 
   it('should initialize the correct chart only once', async(() => {
     builder.createAsync(ChartistComponent).then((fixture: ComponentFixture<ChartistComponent>) => {
-      const chartType = 'Bar';
+      const instance: ChartistComponent = fixture.componentInstance;
+      const chartType: ChartType = 'Bar';
 
       spyOn(Chartist, chartType).and.callThrough();
 
-      fixture.componentInstance.data = data[chartType];
-      fixture.componentInstance.type = chartType;
+      instance.data = data[chartType];
+      instance.type = chartType;
 
-      fixture.componentInstance.renderChart().then(function() {
+      instance.renderChart().then(function(): void {
         expect(Chartist.Bar).toHaveBeenCalledTimes(1);
       });
     });
@@ -42,13 +43,86 @@ describe('chartist component', function() {
 
   it('should return the correct chart instance', async(() => {
     builder.createAsync(ChartistComponent).then((fixture: ComponentFixture<ChartistComponent>) => {
-      const chartType = 'Bar';
+      const instance: ChartistComponent = fixture.componentInstance;
+      const chartType: ChartType = 'Bar';
 
-      fixture.componentInstance.data = data[chartType];
-      fixture.componentInstance.type = chartType;
+      instance.data = data[chartType];
+      instance.type = chartType;
 
-      fixture.componentInstance.renderChart().then(function(chart) {
+      instance.renderChart().then(function(chart: any): void {
         expect(chart instanceof Chartist.Bar).toBe(true);
+      });
+    });
+  }));
+
+  it('should bind events if there are events', async(() => {
+    builder.createAsync(ChartistComponent).then((fixture: ComponentFixture<ChartistComponent>) => {
+      const instance: ChartistComponent = fixture.componentInstance;
+      const chartType: ChartType = 'Bar';
+
+      spyOn(instance, 'bindEvents').and.callThrough();
+
+      instance.data = data[chartType];
+      instance.type = chartType;
+      instance.events = {
+        draw(): boolean {
+          return false;
+        }
+      };
+
+      fixture.detectChanges();
+
+      instance.ngOnInit().then(function(): void {
+        expect(instance.bindEvents).toHaveBeenCalled();
+      });
+    });
+  }));
+
+  it('should re-render the chart if the chart type changes', async(() => {
+    builder.createAsync(ChartistComponent).then((fixture: ComponentFixture<ChartistComponent>) => {
+      const instance: ChartistComponent = fixture.componentInstance;
+      const changes: any = {
+        type: 'Bar'
+      };
+
+      instance.type = 'Line';
+
+      spyOn(instance, 'renderChart').and.callThrough();
+
+      instance.update(changes);
+
+      expect(instance.renderChart).toHaveBeenCalled();
+    });
+  }));
+
+  it('should update the chart if the data changes', async(() => {
+    builder.createAsync(ChartistComponent).then((fixture: ComponentFixture<ChartistComponent>) => {
+      const instance: ChartistComponent = fixture.componentInstance;
+      const changes: any = {
+        data: {
+          labels: [],
+          series: []
+        }
+      };
+
+      instance.data = data['Bar'];
+      instance.type = 'Bar';
+
+      fixture.detectChanges();
+
+      instance.renderChart().then(function(): void {
+        instance.data = data['Line'];
+        instance.type = 'Line';
+
+        spyOn(instance.chart, 'update').and.callThrough();
+        spyOn(instance, 'renderChart').and.callThrough();
+
+        fixture.detectChanges();
+
+        instance.update(changes);
+
+        expect(instance.renderChart).not.toHaveBeenCalled();
+        expect(instance.chart.update).toHaveBeenCalled();
       });
     });
   }));
