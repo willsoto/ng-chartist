ebd7b20f498e44a6c0ef80f718b2a7fd43bbf1f8
Will Soto <will.soto9@gmail.com>
2016-08-18 19:28:01 -0400

    test(entry): fix tests after zone.js update
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- test/entry.ts
+++ test/entry.ts
@@ -2,8 +2,11 @@ import 'es6-shim';
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/proxy-zone';
+import 'zone.js/dist/jasmine-patch';
 import 'rxjs';
 
 import {
