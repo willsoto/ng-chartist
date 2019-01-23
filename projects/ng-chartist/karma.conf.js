96482d7be8ade7991d5d71751a19a666aeae34ed
hakimio <sandshrew@gmail.com>
2019-01-23 15:22:12 +0100

    feat(chartist-tests): improved test console output and unit test structure.

--- projects/ng-chartist/karma.conf.js
+++ projects/ng-chartist/karma.conf.js
@@ -3,29 +3,54 @@
 
 module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
+      require('karma-jasmine-diff-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+
+    // TRIGGER
+    autoWatch: true,
+    singleRun: false,
+
+    // BUILD
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: '',
+    angularCli: {
+      environment: 'dev'
     },
+
+    // RUN
+    port: 9876,
+    browsers: ['ChromeHeadless'],
+
+    // REPORT
+    colors: true,
+    logLevel: config.LOG_INFO,
+
+    reporters: ['jasmine-diff', 'mocha'],
+
+    jasmineDiffReporter: {
+      color: {
+        // white is actually gray here
+        expectedBg: 'bgWhite',
+        expectedWhitespaceBg: 'bgWhite',
+        expectedFg: 'red',
+
+        actualBg: 'bgWhite',
+        actualWhitespaceBg: 'bgWhite',
+        actualFg: 'blue'
+      },
+      legacy: true
+    },
+
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    }
   });
 };
