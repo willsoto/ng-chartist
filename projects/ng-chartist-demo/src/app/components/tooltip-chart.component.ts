34f5cd1d059584cfcab61ef8cf9232a1a0d1751a
hakimio <sandshrew@gmail.com>
2019-06-27 12:30:48 +0200

    fix(tooltip-chart): "appendToBody" seems to fix the tooltip offset.

--- projects/ng-chartist-demo/src/app/components/tooltip-chart.component.ts
+++ projects/ng-chartist-demo/src/app/components/tooltip-chart.component.ts
@@ -5,15 +5,6 @@ import ChartistTooltip from 'chartist-plugin-tooltips-updated';
 
 @Component({
   selector: 'tooltip-chart',
-  styles: [
-    `
-      :host::ng-deep .chartist-tooltip {
-        /* Workaround for incorrect tooltip position in "chartist-plugin-tooltips-updated" version 0.0.21 */
-        margin-left: 20px;
-        margin-top: 55px;
-      }
-    `
-  ],
   template: `
     <h4>Tooltips</h4>
     <x-chartist [data]="data" [type]="type" [options]="options"> </x-chartist>
@@ -26,6 +17,11 @@ export class TooltipChartComponent {
     series: [[12, 9, 7, 8, 5], [2, 1, 3.5, 7, 3], [1, 3, 4, 5, 6]]
   };
   options: ILineChartOptions = {
-    plugins: [ChartistTooltip()]
+    plugins: [
+      ChartistTooltip({
+        anchorToPoint: true,
+        appendToBody: true
+      })
+    ]
   };
 }
