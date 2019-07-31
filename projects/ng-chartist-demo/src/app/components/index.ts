170efdfdd64e972de381627d34977a511c85393d
Tomas Rimkus <sandshrew@gmail.com>
2019-07-31 13:49:31 +0200

    Tooltip plugin usage example (#354)
    * feat(tooltip-chart): added tooltip plugin usage example.
    
    * fix(tooltip-chart): "appendToBody" seems to fix the tooltip offset.
    
    * chore(deps): bump "chartist-plugin-tooltips-updated" package version to 0.1.1.
    
    * docs(tooltip-chart): Added a comment to the tooltip example.

--- projects/ng-chartist-demo/src/app/components/index.ts
+++ projects/ng-chartist-demo/src/app/components/index.ts
@@ -1,3 +1,4 @@
 export { AsyncChartComponent } from './async-chart.component';
 export { DynamicChartComponent } from './dynamic-chart.component';
 export { LiveChartComponent } from './live-chart.component';
+export { TooltipChartComponent } from './tooltip-chart.component';
---
+++ projects/ng-chartist-demo/src/app/components/tooltip-chart.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { ChartType } from 'ng-chartist';
+import { IChartistData, ILineChartOptions } from 'chartist';
+import ChartistTooltip from 'chartist-plugin-tooltips-updated';
+
+/**
+ * Following example shows how to use updated version of Chartist tooltip plugin with ng-chartist
+ * More information about the plugin can be found on github:
+ * https://github.com/LukBukkit/chartist-plugin-tooltip
+ */
+@Component({
+  selector: 'tooltip-chart',
+  template: `
+    <h4>Tooltips</h4>
+    <x-chartist [data]="data" [type]="type" [options]="options"> </x-chartist>
+  `
+})
+export class TooltipChartComponent {
+  type: ChartType = 'Line';
+  data: IChartistData = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    series: [[12, 9, 7, 8, 5], [2, 1, 3.5, 7, 3], [1, 3, 4, 5, 6]]
+  };
+  options: ILineChartOptions = {
+    plugins: [
+      ChartistTooltip({
+        anchorToPoint: true,
+        appendToBody: true
+      })
+    ]
+  };
+}
