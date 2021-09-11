673b6d648d9a60d63f57626ba47a32096f497746
Will Soto <willsoto@users.noreply.github.com>
2021-09-11 09:49:13 -0400

    fix(test): don't sort import due to zone.js requirements
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- projects/ng-chartist/src/test.ts
+++ projects/ng-chartist/src/test.ts
@@ -1,12 +1,11 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+// organize-imports-ignore
+import 'zone.js/dist/zone';
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/dist/zone';
-import 'zone.js/dist/zone-testing';
 
 declare const require: any;
 
